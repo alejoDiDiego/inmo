3e1620fde27fd34abd6918afbd7a6dcbd6ed0e06
Alejo Di Diego <93689456+alejoDiDiego@users.noreply.github.com>
2022-05-21 21:06:31 -0300

    Cambios pagina registro

--- proyecto/components/Register/Register-Empresa/ContainerRegisterEmpresa.js
+++ proyecto/components/Register/Register-Empresa/ContainerRegisterEmpresa.js
@@ -1,33 +1,49 @@
 import React from 'react'
+import Link from 'next/link'
 import styles from '../../../styles/ContainerRegisterEmpresa.module.css'
+
+
 
 const ContainerRegisterEmpresa = () => {
   return (
     <div className={styles.main_container}>
       <div className={styles.inside_container}>
-        <h2>Registrar <span className={styles.text_blue}>tu empresa</span></h2>
+        <h2>Registra <span className={styles.text_blue}>tu empresa</span></h2>
         <form className={styles.form}>
-          <div className={styles.fields}>
-            <label>Nombre de la empresa</label>
-            <input type='text' />
+          <div className={styles.div_fields}>
+            <div className={styles.fields}>
+              <label>Nombre de la empresa</label>
+              <input type='text' />
+            </div>
+
+            <div className={styles.fields}>
+              <label>Email</label>
+              <input type='email' />
+            </div>
+
+            <div className={styles.fields}>
+              <label>Constraseña</label>
+              <input type='password' />
+            </div>
+
+            <div className={styles.fields}>
+              <label>Confirmar contraseña</label>
+              <input type='password' />
+            </div>
           </div>
 
-          <div className={styles.fields}>
-            <label>Email</label>
-            <input type='text' />
+          
+          <div className={styles.div_link}>
+            <label className={styles.label_link}>Permito que utilizen mi informacion segun estos terminos: <Link href='/'><a>www.link.com</a></Link></label>
+            <input type='checkbox' />
           </div>
 
-          <div className={styles.fields}>
-            <label>Constraseña</label>
-            <input type='text' />
-          </div>
 
-          <div className={styles.fields}>
-            <label>Confirmar contraseña</label>
-            <input type='text' />
-          </div>
+
+
+          <input type="submit" value='Siguiente' />
+
         </form>
-        <div></div>
       </div>
     </div>
   )
