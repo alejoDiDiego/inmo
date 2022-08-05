ec98a6b824c896a39cfda7eb6e60fec20e014364
unknown <alejoh2014@gmail.com>
2022-08-05 08:59:40 -0300

    estilos index

--- proyecto/components/Index/Body.js
+++ proyecto/components/Index/Body.js
@@ -7,27 +7,53 @@ import styles from '../../styles/Body.module.css';
 const Body = () => {
     return (
         <div className={styles.div}>
+            <div className={styles.arriba}>
+
+                <div className={styles.inside_arriba}>
+                    <div className={styles.div_izquierda_arriba}>
+                        <div className={styles.div_inside_izquierda_arriba}>
+                            <Link href='/'>
+                                <div className={styles.square}>
+
+                                </div>
+                            </Link>
+
+
+                            <div className={styles.square}>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.div_derecha_arriba}>
+
+                    </div>
+                </div>
+
+            </div>
+
+
             <div className={styles.squares}>
-                <div className={styles.busqueda}>
+                <div className={styles.square_medio}>
                     <div className={styles.img_div}>
                         <img className={styles.img} src="InmoBusqueda.png" />
                     </div>
 
-                    <p className={styles.busqueda_parrafo}>Busqueda simple y resumida, resultados faciles de filtrar, todo al alcance del <span className={styles.span}>usuario.</span></p>
+                    <p className={styles.parrafo}>Busqueda simple y resumida, resultados faciles de filtrar, todo al alcance del <span className={styles.span}>usuario.</span></p>
                 </div>
 
-                <div className={styles.rubik}>
+                <div className={styles.square_medio}>
                     <div className={styles.img_div}>
                         <img className={styles.img} src="InmoRubik.png" />
                     </div>
-                    <p className={styles.rubik_parrafo}>Simplicidad para la creacion de publicaciones, maximo <span className={styles.span}>alcance.</span></p>
+                    <p className={styles.parrafo}>Simplicidad para la creacion de publicaciones, maximo <span className={styles.span}>alcance.</span></p>
                 </div>
 
-                <div className={styles.cuentas}>
+                <div className={styles.square_medio}>
                     <div className={styles.img_div}>
                         <img className={styles.img} src="InmoCuentas.png" />
                     </div>
-                    <p className={styles.cuentas_parrafo}>Incorporacion de cuentas inmobiliarias y empresariales, distincion <span className={styles.span}>instantanea</span></p>
+                    <p className={styles.parrafo}>Incorporacion de cuentas inmobiliarias y empresariales, distincion <span className={styles.span}>instantanea</span></p>
                 </div>
 
             </div>
