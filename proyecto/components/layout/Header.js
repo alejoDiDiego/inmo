8b4d7acaaf4739f9b7c377c57dd83dde2abc37ef
unknown <alejoh2014@gmail.com>
2022-08-05 13:01:41 -0300

    barra de navegacion

--- proyecto/components/layout/Header.js
+++ proyecto/components/layout/Header.js
@@ -2,33 +2,53 @@ import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import firebase from '../../firebase'
 import styles from '../../styles/Header.module.css'
-
+import Image from 'next/image'
 
 
 
 const Header = ({ usuario }) => {
 
+    const [active, setActive] = useState(false)
+
 
     return (
         <div className={styles.header}>
-            {
-                usuario != null ?
-                <button onClick={firebase.handleSignOut}></button>
-                :
-                <div>
-                    <Link href="/inicio-sesion/principal">
-                        <div>
-                            Iniciar Sesion
-                        </div>
-                    </Link>
+            <div className={styles.div_img}>
+                <Image layout="fill" src="/icono_about.png" />
+            </div>
 
-                    <Link href="/registro/principal">
-                        <div>
-                            Registrarse
-                        </div>
-                    </Link>
+            <div className={styles.derecha}>
+
+                <input placeholder='Direccion, localidad, provincia, vendedor' />
+
+
+                <div className={`${styles.menu} ${active == true ? styles.active : null}`} onClick={() => setActive(!active)}>
+                    <div className={styles.bar}></div>
+                    <div className={styles.bar}></div>
+                    <div className={styles.bar}></div>
                 </div>
-            }
+
+
+            </div>
+
+            <div className={`${styles.hidden_menu} ${active == true ? styles.active : null}`}>
+                
+                    <div className={`${styles.inside_hidden_menu} ${active == true ? styles.active : null}`}>
+                        <p>ohalaa</p>
+                        <p>ohalaa</p>
+                        <p>ohalaa</p>
+                        <p>ohalaa</p>
+                        <p>ohalaa</p>
+                        <p>ohalaa</p>
+                    </div>
+                
+            </div>
+
+
+
+
+
+
         </div>
 
     )
