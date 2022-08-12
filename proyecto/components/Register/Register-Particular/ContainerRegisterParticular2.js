ebedd6bbac7cca999d9a5f3ad8318b93470e1343
Alejo Di Diego <93689456+alejoDiDiego@users.noreply.github.com>
2022-08-11 22:09:09 -0300

    crp2 solo falta animacion para abrir el modal

--- proyecto/components/Register/Register-Particular/ContainerRegisterParticular2.js
+++ proyecto/components/Register/Register-Particular/ContainerRegisterParticular2.js
@@ -292,46 +292,53 @@ const ContainerRegisterParticular2 = ({
                                     onRotationChange={setRotation}
                                     onCropComplete={onCropComplete}
                                     onZoomChange={setZoom}
+                                    style={{
+                                        containerStyle: {
+                                            backgroundColor: "white"
+                                        }
+                                    }}
                                 />
                             </div>
-                            <div className={styles.controls}>
-                                <input
-                                    type="range"
-                                    value={zoom}
-                                    min={1}
-                                    max={3}
-                                    step={0.1}
-                                    aria-labelledby="Zoom"
-                                    onChange={(e) => {
-                                        setZoom(e.target.value)
-                                    }}
-                                    className={styles.zoom_range}
-                                />
+                            <div className={styles.div_controls}>
+                                <div className={styles.controls}>
+                                    <input
+                                        type="range"
+                                        value={zoom}
+                                        min={1}
+                                        max={3}
+                                        step={0.1}
+                                        aria-labelledby="Zoom"
+                                        onChange={(e) => {
+                                            setZoom(e.target.value)
+                                        }}
+                                        className={styles.zoom_range}
+                                    />
 
-                                <div className={styles.div_rotate}>
-                                    <button onClick={() => setRotation(rotation - 90)} className={styles.btn_rotate}>
+                                    <div className={styles.div_rotate}>
+                                        <button onClick={() => setRotation(rotation - 90)} className={styles.btn_rotate}>
 
-                                        <Image height={30} width={30} src="/rotate-left.png" />
+                                            <Image height={30} width={30} src="/rotate-left.png" />
 
-                                        {/*https://www.flaticon.com/premium-icon/rotate-left_3889488 */}
-                                    </button>
+                                            {/*https://www.flaticon.com/premium-icon/rotate-left_3889488 */}
+                                        </button>
 
-                                    <button onClick={() => setRotation(rotation + 90)} className={styles.btn_rotate}>
+                                        <button onClick={() => setRotation(rotation + 90)} className={styles.btn_rotate}>
 
-                                        <Image height={30} width={30} src="/rotate-right.png" />
+                                            <Image height={30} width={30} src="/rotate-right.png" />
 
 
-                                        {/*https://www.flaticon.com/premium-icon/rotate-right_3889492 */}
-                                    </button>
+                                            {/*https://www.flaticon.com/premium-icon/rotate-right_3889492 */}
+                                        </button>
+                                    </div>
                                 </div>
 
-
+                                <div className={styles.button_recortar} onClick={showCroppedImagePerfil}>
+                                    <div className={styles.button_back}></div>
+                                    <div className={styles.button_content}><span>Recortar</span></div>
+                                </div>
                             </div>
 
-                            <div className={styles.button_recortar} onClick={showCroppedImagePerfil}>
-                                <div className={styles.button_back}></div>
-                                <div className={styles.button_content}><span>Recortar</span></div>
-                            </div>
+
 
                         </div>
                     </div>
@@ -358,47 +365,54 @@ const ContainerRegisterParticular2 = ({
                                     onRotationChange={setRotation}
                                     onCropComplete={onCropComplete}
                                     onZoomChange={setZoom}
+                                    style={{
+                                        containerStyle: {
+                                            backgroundColor: "white"
+                                        }
+                                    }}
                                 />
                             </div>
-                            <div className={styles.controls}>
-                                <input
-                                    type="range"
-                                    value={zoom}
-                                    min={1}
-                                    max={3}
-                                    step={0.1}
-                                    aria-labelledby="Zoom"
-                                    onChange={(e) => {
-                                        setZoom(e.target.value)
-                                    }}
-                                    className={styles.zoom_range}
-                                />
+                            <div className={styles.div_controls}>
+                                <div className={styles.controls}>
+                                    <input
+                                        type="range"
+                                        value={zoom}
+                                        min={1}
+                                        max={3}
+                                        step={0.1}
+                                        aria-labelledby="Zoom"
+                                        onChange={(e) => {
+                                            setZoom(e.target.value)
+                                        }}
+                                        className={styles.zoom_range}
+                                    />
 
-                                <div className={styles.div_rotate}>
-                                    <button onClick={() => setRotation(rotation - 90)} className={styles.btn_rotate}>
+                                    <div className={styles.div_rotate}>
+                                        <button onClick={() => setRotation(rotation - 90)} className={styles.btn_rotate}>
 
-                                        <Image height={30} width={30} src="/rotate-left.png" />
+                                            <Image height={30} width={30} src="/rotate-left.png" />
 
-                                        {/*https://www.flaticon.com/premium-icon/rotate-left_3889488 */}
-                                    </button>
+                                            {/*https://www.flaticon.com/premium-icon/rotate-left_3889488 */}
+                                        </button>
 
-                                    <button onClick={() => setRotation(rotation + 90)} className={styles.btn_rotate}>
+                                        <button onClick={() => setRotation(rotation + 90)} className={styles.btn_rotate}>
 
-                                        <Image height={30} width={30} src="/rotate-right.png" />
+                                            <Image height={30} width={30} src="/rotate-right.png" />
 
 
-                                        {/*https://www.flaticon.com/premium-icon/rotate-right_3889492 */}
-                                    </button>
+                                            {/*https://www.flaticon.com/premium-icon/rotate-right_3889492 */}
+                                        </button>
+                                    </div>
+
+                                </div>
+                                <div className={styles.button_recortar} onClick={showCroppedImageFondo}>
+                                    <div className={styles.button_back}></div>
+                                    <div className={styles.button_content}><span>Recortar</span></div>
                                 </div>
 
-
-
                             </div>
 
-                            <div className={styles.button_recortar} onClick={showCroppedImageFondo}>
-                                <div className={styles.button_back}></div>
-                                <div className={styles.button_content}><span>Recortar</span></div>
-                            </div>
+
 
                         </div>
                     </div>
