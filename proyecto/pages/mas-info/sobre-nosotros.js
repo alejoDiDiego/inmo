6437644aaf01d5a194a8da0d64e35fa37172285d
MatiasJoaquinAlfonso <joaquinmatias332@gmail.com>
2022-08-17 23:50:00 -0300

    Cambios Heavys
    Descubrimos que puse mal los nombres

---
+++ proyecto/pages/mas-info/sobre-nosotros.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import styles from '../../styles/SobreNosotros.module.css';
+import Layout from '../../components/layout/Layout'
+
+const sobreNosotros = () => {
+    return (
+        <Layout>
+            <div className={styles.body}>
+                <div className={styles.contenedorGrande}>
+                    <div className={styles.contenedorPrincipal}>
+
+                        <div className={styles.img}>
+                            <h1 className={styles.h1}>Sobre <span className={styles.span}>nosostros</span></h1>
+
+
+
+                        </div>
+
+                        <div className={styles.textos}>
+
+                            <h2 className={styles.titulo}>Mas que una simple pagina, un <span className={styles.span}>proyecto</span></h2>
+
+                            <p className={styles.texto}>Nuestro compromiso en inmo es poder traer al usuario una nueva experiencia de busqueda teniendo como puntos centrales la <span className={styles.span}>simplicidad</span> y la <span className={styles.span}>accesibilidad</span>.</p>
+
+                        </div>
+                    </div>
+
+                    <div className={styles.contenedorSecundario}>
+
+                        <div className={styles.imgSec}>
+
+                        </div>
+
+                        <div className={styles.textosSec}>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </Layout>
+
+    )
+}
+
+export default sobreNosotros
