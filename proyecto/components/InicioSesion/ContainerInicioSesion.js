2303c90cf9daeaac49daee41cfc366889ba1aeac
Alejo Di Diego <93689456+alejoDiDiego@users.noreply.github.com>
2022-08-26 18:48:54 -0300

    falta hacer multiples providers por usuario y reestablecer contrasena

--- proyecto/components/InicioSesion/ContainerInicioSesion.js
+++ proyecto/components/InicioSesion/ContainerInicioSesion.js
@@ -1,67 +1,169 @@
 import React from 'react'
 import styles from '../../styles/ContainerInicioSesion.module.css'
 import MyApp from '../../pages/_app'
-import {signInWithEmailAndPassword } from "firebase/auth";
-import {getAuth} from "firebase/auth";
 import Link from 'next/link'
-
-const ContainerInicioSesion = ({
-  email,
-  setEmail,
-  password,
-  setPassword
-}) => {
+import { useState } from 'react'
+import Spinner from '../../components/Spinner/Spinner';
+import Image from 'next/image'
+import firebase from '../../firebase'
 
 
-  const handlerIniciarSesion = () => {
-    const auth = getAuth(MyApp.app)
-    const emailRegistro = email
-    const passwordRegistro = password
-    signInWithEmailAndPassword(auth, emailRegistro, passwordRegistro).then((userCredential) => {
-    const user = userCredential.user;
-    console.log(user)
-    }).catch((error) => {
-      const errorCode = error.code;
-      console.log(errorCode)
-      const errorMessage = error.message; 
-      console.log(errorMessage) 
-    });
+const ContainerInicioSesion = ({ loadingBig }) => {
 
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [viewPassword, setViewPassword] = useState(false)
+
+  const [loading, setLoading] = useState(false)
+
+
+  const handleGoogle = async () => {
+    try {
+      setLoading(true)
+      await firebase.registrarGoogle();
+    } catch {
+      setLoading(false)
+    }
+
+  }
+
+
+
+  const handleIniciarSesion = async () => {
+    let respuesta
+    try {
+      setLoading(true)
+      respuesta = await firebase.iniciarSesion(email, password)
+      if(respuesta != ""){
+        setLoading(false)
+        console.log(respuesta)
+      }
+    } catch (err) {
+      setLoading(false)
+      console.log(err.code.message) 
+    }
   }
 
 
   return (
 
-    <div className={styles.main_container}>
 
-        <h2 className={styles.h2}>Bienvenido a <span className={styles.blue}>Inmo</span></h2>
+    <div className={styles.div_supremo}>
+      <div className={styles.main_container}>
+        {
+          loadingBig == false ?
+          (
+            <div className={styles.inside_container}>
+              <h2>Inicia <span className={styles.text_blue}>Sesion</span></h2>
+              <div className={styles.form}>
 
-          <div className ={styles.fieldsE}>
-            <label>Correo electronico</label>
-            <input value={email} onChange={e => setEmail(e.target.value)} type='email'/>
-          </div>
+                <label className={`${styles.custom_field} ${styles.two}`}>
+                  <input value={email} onChange={e => { setEmail(e.target.value); }} type="text" readOnly={loading} placeholder="&nbsp;" />
+                  <span className={styles.placeholder}>Email</span>
+                </label>
 
-          <div className={styles.fieldsC}>
-            <label>Contraseña</label>
-            <input value={password} onChange={e => setPassword(e.target.value)} type='text'/>
-          </div>
+                <label className={`${styles.custom_field} ${styles.two}`}>
+                  <input value={password} id="password" onChange={e => { setPassword(e.target.value); }} type={viewPassword == true ? "text" : "password"} readOnly={loading} placeholder="&nbsp;" />
+                  <span className={styles.placeholder}>Contraseña</span>
+                  <i onClick={() => setViewPassword(!viewPassword)} class={viewPassword == true ? "fa-solid fa-eye" : "far fa-eye"}></i>
+                </label>
 
-          <div className={styles.div_man}>
-            <p className={styles.man}>Mantener sesion iniciada</p>   
-            <input type="checkbox" /> 
-          </div>
 
-        <div className={styles.div_problemasInicio}>
-          <p className={styles.problemasInicio}> Problemas para inciar sesion?</p>
-          </div>
+                {
+                  loading == false ?
+                    <div className={styles.div_buttons}>
+                      {/*<button className={styles.buttonGoogle} onClick={handleGoogle}><span>Google</span><Image src='/google.png' width={25} height={25} /></button> <a href="https://www.flaticon.es/iconos-gratis/google" title="google iconos">Google iconos creados por Freepik - Flaticon</a>*/}
+                      <div className={styles.buttonGoogle} onClick={handleGoogle}>
+                        <div className={styles.buttonGoogle_back}></div>
+                        <div className={styles.buttonGoogle_content}><span>Iniciar sesion con Google</span><Image src='/google.png' width={25} height={25} /></div>
+                      </div>
+                      <div className={styles.button} onClick={handleIniciarSesion}>
+                        <div className={styles.button_back}></div>
+                        <div className={styles.button_content}><span>Iniciar Sesion</span></div>
+                      </div>
+                    </div>
+                    :
+                    <div className={styles.div_spinner}>
+                      <Spinner />
+                    </div>
 
-          <div className={styles.div_crearCuenta}>
-          <p className={styles.crearCuenta}> Aun no estas regristrado en inmo?</p>
-          </div>
 
-        <button onClick={handlerIniciarSesion} className={styles.button}>Iniciar Sesion</button>
+                }
 
+              </div>
+            </div>
+          ) : 
+          (
+            <Spinner />
+          )
+        }
+
+      </div>
     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div className={styles.main_container}>
+
+    //     <h2 className={styles.h2}>Bienvenido a <span className={styles.blue}>Inmo</span></h2>
+
+    //       <div className ={styles.fieldsE}>
+    //         <label>Correo electronico</label>
+    //         <input value={email} onChange={e => setEmail(e.target.value)} type='email'/>
+    //       </div>
+
+    //       <div className={styles.fieldsC}>
+    //         <label>Contraseña</label>
+    //         <input value={password} onChange={e => setPassword(e.target.value)} type='text'/>
+    //       </div>
+
+    //       <div className={styles.div_man}>
+    //         <p className={styles.man}>Mantener sesion iniciada</p>   
+    //         <input type="checkbox" /> 
+    //       </div>
+
+    //     <div className={styles.div_problemasInicio}>
+    //       <p className={styles.problemasInicio}> Problemas para inciar sesion?</p>
+    //       </div>
+
+    //       <div className={styles.div_crearCuenta}>
+    //       <p className={styles.crearCuenta}> Aun no estas regristrado en inmo?</p>
+    //       </div>
+
+    //     <button onClick={handlerIniciarSesion} className={styles.button}>Iniciar Sesion</button>
+
+    // </div>
   )
 }
 
--- proyecto/firebase/firebase.js
+++ proyecto/firebase/firebase.js
@@ -1,53 +1,63 @@
 import { initializeApp } from 'firebase/app';
-import { createUserWithEmailAndPassword, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, getAuth, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from 'firebase/firestore'
-import { GoogleAuthProvider } from "firebase/auth";
 import "firebase/compat/firestore";
 import firebaseConfig from './config';
 
 
-
-
 class Firebase {
 
-    
-
-    constructor() {
-        this.app = initializeApp(firebaseConfig);
-        this.auth = getAuth(this.app)
-        this.auth.languageCode = 'es_419';
-        this.auth.useDeviceLanguage();
-
-        this.storage = getStorage(this.app)
-
-        this.db = getFirestore(this.app)
-
-        this.providerGoogle = new GoogleAuthProvider();
 
 
-    }
+  constructor() {
+    this.app = initializeApp(firebaseConfig);
+    this.auth = getAuth(this.app)
+    this.auth.languageCode = 'es_419';
+    this.auth.useDeviceLanguage();
 
-    async registrar(auth, email, password) {
-        const nuevoUsuario = await createUserWithEmailAndPassword(auth, email, password)
-        return nuevoUsuario.user
-    }
+    this.storage = getStorage(this.app)
 
-    async registrarGoogle() {
-        const nuevoUsuario = await signInWithPopup(this.auth, this.providerGoogle)
-        return nuevoUsuario.user 
-    }
+    this.db = getFirestore(this.app)
 
-    handleSignOut = () => {
-        signOut(this.auth).then(() => {
-          console.log("Sign-out successful.")
-          console.log(this.auth)
-          window.location.href = "http://localhost:3000//";
-        }).catch((error) => {
-          console.log("No se deslogueo")
-        });
-      
-      }
+    this.providerGoogle = new GoogleAuthProvider();
+
+
+  }
+
+  async registrar(auth, email, password) {
+    const nuevoUsuario = await createUserWithEmailAndPassword(auth, email, password)
+    return nuevoUsuario.user
+  }
+
+  async registrarGoogle() {
+    const nuevoUsuario = await signInWithPopup(this.auth, this.providerGoogle)
+    return nuevoUsuario.user
+  }
+
+  async iniciarSesion(email, password) {
+    signInWithEmailAndPassword(this.auth, email, password).then((userCredential) => {
+      const user = userCredential.user;
+      return user
+    }).catch((error) => {
+      const errorCode = error.code;
+      console.log(errorCode)
+      const errorMessage = error.message;
+      console.log(errorMessage)
+      return errorMessage
+    });
+  }
+
+  handleSignOut = () => {
+    signOut(this.auth).then(() => {
+      console.log("Sign-out successful.")
+      console.log(this.auth)
+      window.location.href = "http://localhost:3000//";
+    }).catch((error) => {
+      console.log("No se deslogueo")
+    });
+
+  }
 }
 
 const firebase = new Firebase();
