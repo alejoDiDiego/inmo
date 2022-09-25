84a985f9f0d27fb48bd87c9937de61ff9ccf460f
Alejo Di Diego <93689456+alejoDiDiego@users.noreply.github.com>
2022-09-25 16:39:33 -0300

    modificar y eliminar imagenes subidas

---
+++ proyecto/components/CrearPublicacionPrincipal/SubirImagenes.js
@@ -0,0 +1,250 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { getCroppedImg, getRotatedImage } from '../../crop/auxCrop'
+import Cropper from 'react-easy-crop'
+import styles from '../../styles/SubirImagenes.module.css'
+import Image from 'next/image'
+
+
+
+const SubirImagenes = ({
+    imagenes,
+    setImagenes
+}) => {
+
+
+
+    const [cargandoCorte, setCargandoCorte] = useState(false)
+    const [imageSrc, setImageSrc] = React.useState(null)
+    const [crop, setCrop] = useState({ x: 0, y: 0 })
+    const [rotation, setRotation] = useState(0)
+    const [zoom, setZoom] = useState(1)
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+
+    const [modalPerfil, setModalPerfil] = useState(false)
+
+    const [imagePerfilUpload, setImagePerfilUpload] = useState([])
+
+
+    const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+        setCroppedAreaPixels(croppedAreaPixels)
+    }, [])
+
+
+
+    const showCroppedImagePerfil = useCallback(async () => {
+        try {
+            setCargandoCorte(true)
+            const croppedImage = await getCroppedImg(
+                imageSrc,
+                croppedAreaPixels,
+                rotation
+            )
+            console.log('donee', { croppedImage })
+
+
+            let blobPerfil = await fetch(croppedImage).then(r => r.blob());
+
+            setImagenes([...imagenes, blobPerfil])
+            setCargandoCorte(false)
+            
+
+
+            setImageSrc(null)
+            setCrop({ x: 0, y: 0 })
+            setRotation(0)
+            setZoom(1)
+            setCroppedAreaPixels(null)
+            setModalPerfil(false)
+            setImagePerfilUpload([])
+            setImageFondoUpload([])
+
+
+        } catch (e) {
+            console.error(e)
+        }
+    }, [imageSrc, croppedAreaPixels, rotation])
+
+
+    useEffect(() => {
+        console.log(imagenes)
+    }, [imagenes])
+
+
+
+
+
+    useEffect(() => {
+        if (typeof imagePerfilUpload == "undefined" || imagePerfilUpload.length == 0) {
+            return
+        }
+        const change = async () => {
+            console.log(imagePerfilUpload)
+            const file = imagePerfilUpload
+            console.log("llega1")
+            let imageDataUrl = await readFile(file)
+
+            setImageSrc(imageDataUrl)
+            setModalPerfil(true)
+            console.log("llega2")
+        }
+        change()
+        return
+    }, [imagePerfilUpload])
+
+
+
+    function readFile(file) {
+        return new Promise((resolve) => {
+            const reader = new FileReader()
+            reader.addEventListener('load', () => resolve(reader.result), false)
+            reader.readAsDataURL(file)
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return (
+        <div>
+            <div>
+                <h2>Subir Imagenes</h2>
+
+                <div>
+                    <label htmlFor='file-img-perfil'>Subir imagen</label>
+                    <input className={styles.input_file} onChange={(e) => { setImagePerfilUpload(e.target.files[0]) }} accept="image/png, image/gif, image/jpeg" type="file" id='file-img-perfil' />
+                </div>
+
+                {
+                    imagenes.map(m => {
+                        let urlCreator = window.URL || window.webkitURL;
+                        let index = imagenes.indexOf(m)
+                        console.log(index)
+                        return(
+                            <div className={styles.div_img}>
+                                <div className={styles.iconos_img}>
+                                    <img src='/edit.png' className={styles.edit_icon} />
+                                    <img src='/delete.png' className={styles.delete_icon} />
+                                </div>
+                                <img className={styles.img_subir} key={index} src={urlCreator.createObjectURL(m)} />
+                            </div>
+                        )
+                    })
+                }
+
+
+
+                {
+                    modalPerfil ?
+                        <div className={styles.modal_crop}>
+                            <div className={styles.modal_crop_inside}>
+                                {
+                                    cargandoCorte == false ?
+                                        (
+                                            <div className={styles.menu} onClick={() => { setModalPerfil(false); setImagePerfilUpload([]); setCroppedAreaPixels(null); setZoom(1); setRotation(0) }}>
+                                                <div className={styles.bar}></div>
+                                                <div className={styles.bar}></div>
+                                                <div className={styles.bar}></div>
+                                            </div>
+                                        ) : <div className={styles.menu_cargando}></div>
+                                }
+
+                                <div className={styles.div_cropper}>
+                                    <Cropper
+                                        image={imageSrc}
+                                        crop={crop}
+                                        rotation={rotation}
+                                        zoom={zoom}
+                                        aspect={1 / 1}
+                                        onCropChange={setCrop}
+                                        onRotationChange={setRotation}
+                                        onCropComplete={onCropComplete}
+                                        onZoomChange={setZoom}
+                                        style={{
+                                            containerStyle: {
+                                                backgroundColor: "white"
+                                            }
+                                        }}
+                                    />
+                                </div>
+
+
+                                {
+                                    cargandoCorte == false ?
+                                        (
+                                            <div className={styles.div_controls}>
+                                                <div className={styles.controls}>
+                                                    <input
+                                                        type="range"
+                                                        value={zoom}
+                                                        min={1}
+                                                        max={3}
+                                                        step={0.1}
+                                                        aria-labelledby="Zoom"
+                                                        onChange={(e) => {
+                                                            setZoom(e.target.value)
+                                                        }}
+                                                        className={styles.zoom_range}
+                                                    />
+
+                                                    <div className={styles.div_rotate}>
+                                                        <button onClick={() => setRotation(rotation - 90)} className={styles.btn_rotate}>
+
+                                                            <Image height={30} width={30} src="/rotate-left.png" />
+
+                                                            {/*https://www.flaticon.com/premium-icon/rotate-left_3889488 */}
+                                                        </button>
+
+                                                        <button onClick={() => setRotation(rotation + 90)} className={styles.btn_rotate}>
+
+                                                            <Image height={30} width={30} src="/rotate-right.png" />
+
+
+                                                            {/*https://www.flaticon.com/premium-icon/rotate-right_3889492 */}
+                                                        </button>
+                                                    </div>
+                                                </div>
+
+                                                <div className={styles.button_recortar} onClick={showCroppedImagePerfil}>
+                                                    <div className={styles.button_back}></div>
+                                                    <div className={styles.button_content}><span>Recortar</span></div>
+                                                </div>
+                                            </div>
+
+
+                                        ) :
+                                        (
+                                            <div className={styles.div_controls}>
+                                                <p>cargando</p>
+                                            </div>
+                                        )
+
+                                }
+
+
+
+
+                            </div>
+                        </div>
+                        : null
+                }
+
+
+            </div>
+        </div>
+    )
+}
+
+export default SubirImagenes
--- proyecto/pages/crear-publicacion/principal.js
+++ proyecto/pages/crear-publicacion/principal.js
@@ -5,6 +5,9 @@ import Layout from '../../components/layout/Layout'
 import Head from 'next/head'
 import styles from '../../styles/CrearPublicacion.module.css'
 import InformacionBasica from '../../components/CrearPublicacionPrincipal/InformacionBasica'
+import Descripcion from '../../components/CrearPublicacionPrincipal/Descripcion'
+import SubirImagenes from '../../components/CrearPublicacionPrincipal/SubirImagenes'
+
 
 const principal = () => {
 
@@ -31,6 +34,15 @@ const principal = () => {
   const [tipoPublicacion, setTipoPublicacion] = useState("")
   const [precio, setPrecio] = useState("")
   const [expensas, setExpensas] = useState("")
+
+
+
+  const [titulo, setTitulo] = useState("")
+  const [descripcion, setDescripcion] = useState("")
+
+
+
+  const [imagenes, setImagenes] = useState([])
 
 
 
@@ -105,40 +117,53 @@ const principal = () => {
             </div>
 
 
-                  <div className={styles.derecha}>
-                    <InformacionBasica
-                      provincia={provincia}
-                      setProvincia={setProvincia}
-                      municipio={municipio}
-                      setMunicipio={setMunicipio}
-                      localidad={localidad}
-                      setLocalidad={setLocalidad}
-                      direccion={direccion}
-                      setDireccion={setDireccion}
-                      codigoPostal={codigoPostal}
-                      setCodigoPostal={setCodigoPostal}
-                      altura={altura}
-                      setAltura={setAltura}
-                      latLon={latLon}
-                      setLatLon={setLatLon}
-                      pisoDepto={pisoDepto}
-                      setPisoDepto={setPisoDepto}
-                      setTipoVivienda={setTipoVivienda}
-                      cantAmbientes={cantAmbientes}
-                      setCantAmbientes={setCantAmbientes}
-                      cantBanos={cantBanos}
-                      setCantBanos={setCantBanos}
-                      cantHabitaciones={cantHabitaciones}
-                      setCantHabitaciones={setCantHabitaciones}
-                      cantCocheras={cantCocheras}
-                      setCantCocheras={setCantCocheras}
-                      setTipoPublicacion={setTipoPublicacion}
-                      precio={precio}
-                      setPrecio={setPrecio}
-                      expensas={expensas}
-                      setExpensas={setExpensas}
-                    />
-                  </div>
+            <div className={styles.derecha}>
+              <InformacionBasica
+                provincia={provincia}
+                setProvincia={setProvincia}
+                municipio={municipio}
+                setMunicipio={setMunicipio}
+                localidad={localidad}
+                setLocalidad={setLocalidad}
+                direccion={direccion}
+                setDireccion={setDireccion}
+                codigoPostal={codigoPostal}
+                setCodigoPostal={setCodigoPostal}
+                altura={altura}
+                setAltura={setAltura}
+                latLon={latLon}
+                setLatLon={setLatLon}
+                pisoDepto={pisoDepto}
+                setPisoDepto={setPisoDepto}
+                setTipoVivienda={setTipoVivienda}
+                cantAmbientes={cantAmbientes}
+                setCantAmbientes={setCantAmbientes}
+                cantBanos={cantBanos}
+                setCantBanos={setCantBanos}
+                cantHabitaciones={cantHabitaciones}
+                setCantHabitaciones={setCantHabitaciones}
+                cantCocheras={cantCocheras}
+                setCantCocheras={setCantCocheras}
+                setTipoPublicacion={setTipoPublicacion}
+                precio={precio}
+                setPrecio={setPrecio}
+                expensas={expensas}
+                setExpensas={setExpensas}
+              />
+
+              <Descripcion
+                titulo={titulo}
+                setTitulo={setTitulo}
+                descripcion={descripcion}
+                setDescripcion={setDescripcion}
+              />
+
+              <SubirImagenes
+                imagenes={imagenes}
+                setImagenes={setImagenes}
+              />
+
+            </div>
 
 
 
