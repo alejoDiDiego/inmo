cba40ec7cda8d2e8f946bb5451846af551acd0a7
Alejo Di Diego <93689456+alejoDiDiego@users.noreply.github.com>
2022-10-02 16:10:38 -0300

    buscar y map

---
+++ proyecto/components/layout/InputBusqueda.js
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import Router from 'next/router'
+import React, { useState } from 'react'
+import styles from "../../styles/InputBusqueda.module.css"
+
+const InputBusqueda = () => {
+
+    const [busqueda, setBusqueda] = useState("")
+
+
+
+    const handleBuscar = () => {
+        if(busqueda.trim() === "") return
+
+        Router.push({
+            pathname: '/publicaciones/principal',
+            query: { q : busqueda}
+        })
+    }
+
+
+
+
+    return (
+        <div className={styles.main}>
+            <input value={busqueda} onChange={e => setBusqueda(e.target.value)} className={styles.input} placeholder='Direccion, localidad, provincia, vendedor' />
+            <div onClick={() => handleBuscar()} className={styles.div_img}>
+                <Image src='/search2.png' layout='fill' />
+            </div>
+
+        </div>
+    )
+}
+
+export default InputBusqueda
---
+++ proyecto/pages/publicaciones/principal.js
@@ -0,0 +1,190 @@
+import React, { useContext, useEffect, useState } from 'react'
+import Layout from '../../components/layout/Layout'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import firebase, { FirebaseContext } from '../../firebase'
+import { doc, getDoc, updateDoc, collection, query, where, getDocs } from 'firebase/firestore'
+import dynamic from 'next/dynamic'
+import styles from "../../styles/PublicacionesPrincipal.module.css"
+
+const MapNoSSR = dynamic(() => import("../../components/Publicaciones/Map"), {
+    ssr: false,
+});
+
+
+const principal = () => {
+
+    const { usuario } = useContext(FirebaseContext)
+    const [loading, setLoading] = useState(true)
+
+    const router = useRouter()
+    const { query: { q } } = router
+
+    const [publicaciones, setPublicaciones] = useState([])
+    const [resultado, setResultado] = useState([])
+
+    const [positions, setPositions] = useState([])
+
+
+
+
+
+
+    useEffect(() => {
+
+        const check = async () => {
+            if (usuario != null) {
+                try {
+                    queryFirebase()
+                    if (Object.keys(usuario).length > 0) {
+                        setLoading(false)
+                    } else {
+                        setLoading(false)
+                    }
+                    return true
+
+                } catch (err) {
+                    console.log(err)
+                    console.log("a chekear")
+                    setTimeout(() => {
+                        check()
+                        return
+                    }, 2000)
+                }
+            } else {
+                return false
+            }
+        }
+
+        //Op
+
+        let prueba = check()
+        while (prueba == false) {
+            setInterval(() => {
+                prueba = check()
+                console.log("probando")
+            }, 200)
+
+        }
+
+
+
+    }, [usuario])
+
+
+
+
+
+
+
+
+
+    useEffect(() => {
+
+        if (q == "" || q == null) return
+        if (publicaciones.length == 0) return
+
+        const busqueda = q.toLowerCase()
+        const filtro = publicaciones.filter(p => {
+            return (
+                p.provincia.toLowerCase().includes(busqueda) ||
+                p.municipio.toLowerCase().includes(busqueda) ||
+                p.localidad.toLowerCase().includes(busqueda) ||
+                p.type.toLowerCase().includes(busqueda) ||
+                p.tipoVivienda.toLowerCase().includes(busqueda) ||
+                p.descripcion.toLowerCase().includes(busqueda)
+            )
+        })
+        setResultado(filtro)
+
+    }, [q, publicaciones])
+
+
+    useEffect(() => {
+        if (resultado.length == 0) return
+
+        const posiciones = resultado.map(p => {
+            return ({
+                id: p.id,
+                latLon: p.latLon
+            })
+        })
+        console.log(posiciones)
+        setPositions(posiciones)
+    }, [resultado])
+
+
+
+
+
+
+
+
+    const queryFirebase = async () => {
+        const colRef = collection(firebase.db, "Publicaciones")
+        const querySnapshot = await getDocs(colRef);
+        let ps = []
+        querySnapshot.forEach((doc) => {
+            ps.push(doc.data())
+        });
+        setPublicaciones(ps)
+        return true
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if (loading == true) {
+        return (
+            <>
+                <Head>
+                    <title>Inmo</title>
+                    <meta name="description" content="Generated" />
+                    <link rel="icon" href="/Logo_inmo_new.png" />
+                </Head>
+                <Layout>
+                    <p>Cargando</p>
+                </Layout>
+            </>
+        )
+    }
+
+    else {
+        return (
+            <>
+                <Head>
+                    <title>Inmo</title>
+                    <meta name="description" content="Generated" />
+                    <link rel="icon" href="/Logo_inmo_new.png" />
+                </Head>
+                <Layout perfil={true}>
+                    <div className={styles.main}>
+                        <div className={styles.izquierda}>
+                            <MapNoSSR
+                                positions={positions}
+                            />
+                        </div>
+
+                        <div className={styles.derecha}>
+
+                        </div>
+                    </div>
+                </Layout>
+            </>
+        )
+    }
+}
+
+export default principal
