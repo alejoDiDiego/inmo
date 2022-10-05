2c7dca8948faffe66b029dd0f1aff3e9a7024b8d
Alejo Di Diego <93689456+alejoDiDiego@users.noreply.github.com>
2022-10-04 21:04:03 -0300

    publicador en publicacion expandida

--- proyecto/components/Publicaciones/PublicacionExtendida.js
+++ proyecto/components/Publicaciones/PublicacionExtendida.js
@@ -1,21 +1,33 @@
 import { doc, getDoc } from 'firebase/firestore'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import firebase from '../../firebase'
 import styles from "../../styles/PublicacionExtendida.module.css"
 
 
 const PublicacionExtendida = ({ p }) => {
 
+    const [publicador, setPublicador] = useState({})
+
+
+    let puntajeEstrellas = 0
 
     const queryFirebase = async () => {
         const docRef = doc(firebase.db, "Usuarios", p.publicador)
         const docSnap = await getDoc(docRef)
         console.log(docSnap.data())
+        docSnap.data().valoraciones.map(v => {
+            puntajeEstrellas += v.estrellas
+        })
+        setPublicador(docSnap.data())
+
     }
 
     useEffect(() => {
-        queryFirebase()
-    })
+        if(!publicador.hasOwnProperty("creado")) {
+            queryFirebase()
+        }
+    }, [publicador])
+
 
 
 
@@ -35,6 +47,41 @@ const PublicacionExtendida = ({ p }) => {
             <div className={styles.div_descripcion}>
                 <p>{p.descripcion}</p>
             </div>
+
+            {
+                !publicador.hasOwnProperty("creado") ? null :
+
+                    (
+                        <div className={styles.publicador}>
+                            <img src={publicador.fotoPerfilURL} />
+                            <div>
+                                <p>{publicador.nombreUsuario}</p>
+                                <p>{publicador.type}</p>
+                                <p>{publicador.numeroCelular}</p>
+
+                            </div>
+                            {
+                                publicador.numeroTelefono.length == 0 ? null :
+                                    <p>{publicador.numeroTelefono}</p>
+                            }
+
+                            {
+                                publicador.emailPublico == true &&
+                                <p>{publicador.mail}</p>
+
+                            }
+                            {
+
+                                publicador.valoraciones.length == 0 ?
+                                    <p>0 Estrellas de 0 Valoraciones.</p>
+                                    :
+                                    <p>{puntajeEstrellas / publicador.valoraciones.length} Estrellas de {publicador.valoraciones.length} Valoraciones</p>
+                            }
+
+
+                        </div>
+                    )
+            }
         </div>
     )
 }
